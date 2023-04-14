c1c4db9c7e6414a23c4bd0927ef8c0656e5eea7b
서수교 <sk.seo@navercorp.com>
2023-04-14 18:44:16 +0900

    20230414-학습

--- react-course/src/App.tsx
+++ react-course/src/App.tsx
@@ -1,36 +1,19 @@
-import Todo from './components/Todo';
-import { useRef, useState } from 'react';
-
-interface InputItem {
-	id: number;
-	title: string;
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetupsPage';
+import NewMeetupPage from './pages/NewMeetupPage';
+import FavoritesPage from './pages/FavoritesPage';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
-	const [todoList, setTodoList] = useState([]);
-	const nextID = useRef<number>(1);
-	// const [inputItems, setInputItems] = useState<InputItem[]>([{ id: 0, title: '' }]);
-	const [inputItems, setInputItems] = useState<string[]>([]);
-
-	const addTodos = () => {
-		setInputItems([...inputItems, nextID.toString()]); // 기존 값에 새로운 인풋객체를 추가해준다.
-		nextID.current += 1; // id값은 1씩 늘려준다.
-	};
-
 	return (
-		<>
-			<div>
-				<h1>My Todos</h1>
-				<Todo text="Learn React" />
-				<Todo text="Master React" />
-				<Todo text="Explore the full React course" />
-			</div>
-
-			{/*{inputItems.map((item, index) => (
-				<Todo text="New todo" />
-			))}
-			<button onClick={addTodos}>+</button>*/}
-		</>
+		<div>
+			<MainNavigation />
+			<Routes>
+				<Route path="/" element={<AllMeetupsPage />} />
+				<Route path="/new-meetup" element={<NewMeetupPage />} />
+				<Route path="/favorites" element={<FavoritesPage />} />
+			</Routes>
+		</div>
 	);
 }
 
---
+++ react-course/src/components/layout/MainNavigation.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './MainNavigation.module.css';
+// import './mainNavigation.css';
+
+const MainNavigation = () => {
+	return (
+		<header className={style.header}>
+			<div className={style.logo}>React Meetups</div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">All Meetups</Link>
+					</li>
+					<li>
+						<Link to="/new-meetup">Add New Meetup</Link>
+					</li>
+					<li>
+						<Link to="/favorites">My Favorites</Link>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default MainNavigation;
