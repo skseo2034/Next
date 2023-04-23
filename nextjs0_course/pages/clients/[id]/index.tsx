d91a7dc6a741e6aefefb37c6f95ef17f09e97713
서수교 <sk.seo@navercorp.com>
2023-04-23 10:22:38 +0900

    20230423-학습

--- nextjs0_course/pages/clients/[id]/index.tsx
+++ nextjs0_course/pages/clients/[id]/index.tsx
@@ -5,9 +5,21 @@ const ClientProjectsPage = () => {
 	const router = useRouter();
 	console.log('clients > [id] > index : ClientProjectsPage', router.query);
 
+	const loadProjectHandler = () => {
+		// load data...
+		router
+			.push({
+				pathname: '/clients/[id]/[clientprojectid]',
+				query: { id: 'max', clientprojectid: 'projecta' },
+			})
+			.then(r => console.log(r))
+			.catch(e => console.log('aaaa', e.message));
+		/*router.push('/clients/max/projecta').then();*/
+	};
 	return (
 		<div>
 			<h1>The Projects of a Given Client</h1>
+			<button onClick={loadProjectHandler}>Load Project A</button>
 		</div>
 	);
 };
--- nextjs0_course/pages/clients/index.tsx
+++ nextjs0_course/pages/clients/index.tsx
@@ -13,7 +13,8 @@ const ClientPage = () => {
 			<ul>
 				{clients.map(client => (
 					<li key={client.id}>
-						<Link href={`clients/${client.id}`}>{client.name}</Link>
+						{/*<Link href={`clients/${client.id}`}>{client.name}</Link>*/}
+						<Link href={{ pathname: '/clients/[id]', query: { id: client.id } }}>{client.name}</Link>
 					</li>
 				))}
 				{/*<li>
