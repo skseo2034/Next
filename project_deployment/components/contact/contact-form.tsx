9499050f1fe7d8f04cfb214653bba161b70fd0eb
서수교 <sk.seo@navercorp.com>
2023-06-12 11:02:17 +0900

    20230612-학습2

--- project_deployment/components/contact/contact-form.tsx
+++ project_deployment/components/contact/contact-form.tsx
@@ -74,7 +74,7 @@ const ContactForm = () => {
 		notification = {
 			status: 'success',
 			title: 'Success!',
-			message: 'Message sent successfully!',
+			message: 'Message sent successfully!!!!!',
 		};
 	}
 
