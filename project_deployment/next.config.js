f69997a7d16e6183c06734cdb061baffbd4be9a4
서수교 <sk.seo@navercorp.com>
2023-06-14 11:11:10 +0900

    20230614-학습

--- project_deployment/next.config.js
+++ project_deployment/next.config.js
@@ -8,9 +8,13 @@ const {
 	// eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require('next/constants');
 
+const isLocal = process.env.NODE_ENV === 'local';
+console.log('seo >>>>>>>>>>>>> ', isLocal);
+
 module.exports = phase => {
 	if (phase === PHASE_DEVELOPMENT_SERVER) {
 		return {
+			assetPrefix: isLocal ? './' : '',
 			reactStrictMode: true,
 			env: {
 				mongodb_username: 'skseo',
@@ -18,15 +22,22 @@ module.exports = phase => {
 				mongodb_clustername: 'cluster1',
 				mongodb_database: 'my-site-dev',
 			},
+			images: {
+				unoptimized: true,
+			},
 		};
 	}
 	return {
+		assetPrefix: isLocal ? './' : '',
 		reactStrictMode: true,
 		env: {
 			mongodb_username: 'skseo',
 			mongodb_password: 'OBrGWLrzEnsL8ixL',
 			mongodb_clustername: 'cluster1',
 			mongodb_database: 'my-site',
+		},
+		images: {
+			unoptimized: true,
 		},
 	};
 };
